151a38c82e1e0884252af081aef19467af7b7179
randyfortier <randy.fortier@gmail.com>
2019-11-01 17:53:59 -0400

    Added unit testing demos

--- Week08/testing.py
+++ Week08/testing.py
@@ -12,3 +12,22 @@ class Student:
         for mark in self.marks:
             sum += mark
         return sum / len(self.marks)
+
+import unittest 
+
+class StudentTest(unittest.TestCase):
+    def setUp(self):
+        self.carla = Student(4.0, 'Carla Rodriguez')
+
+    def test_set_mark(self):
+        self.carla.set_mark('CSCI 1030U', 75.0)
+        self.assertEqual(len(self.carla.marks), 1)
+        self.assertEqual(self.carla.marks[0], 75.0)
+
+    def test_get_average(self):
+        self.carla.set_mark('CSCI 1030U', 70.0)
+        self.carla.set_mark('CSCI 1060U', 60.0)
+        self.carla.set_mark('MATH 1010U', 80.0)
+        self.assertEqual(self.carla.get_average(), 70.0)
+
+unittest.main()  # execute the tests
